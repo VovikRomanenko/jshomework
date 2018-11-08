515c464fc1876b855887240498eaa2c3fd0c653b
VovikRomanenko <38104364+VovikRomanenko@users.noreply.github.com>
2018-11-08 12:18:41 +0200

    homework 1920

---
+++ Homework 19-20/js/javascript.js
@@ -0,0 +1,9 @@
+$('#example1, #example3').accordion();
+$('#example2').accordion({
+		canToggle: true
+});
+$('#example4').accordion({
+		canToggle: true,
+		canOpenMultiple: true
+});
+$(".loading").removeClass("loading");
